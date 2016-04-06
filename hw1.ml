75649764bd8bfaae57526b8d78ecd689f56eae2a
Jason Jiang <jiang.jason.x@gmail.com>
2016-04-06 09:58:11 -0700

    1a complete

--- hw1.ml
+++ hw1.ml
@@ -3,10 +3,13 @@ exception ImplementMe
 
 (* Problem 1 *)
             
-let (member : 'a -> 'a list -> bool) =
-  raise ImplementMe
+let rec  (member : 'a -> 'a list -> bool) =
+  fun x s ->
+  match s with 
+	[] -> false
+|	h::t -> if x=h then true else (member x t);; 
 
-let (add : 'a -> 'a list -> 'a list) =
+(*let (add : 'a -> 'a list -> 'a list) =
   raise ImplementMe
 
 let (union : 'a list -> 'a list -> 'a list) =
@@ -35,4 +38,4 @@ let (whle : ('a -> bool) -> ('a -> 'a) -> 'a -> 'a) =
                                     
 let (pow : int -> ('a -> 'a) -> ('a -> 'a)) =
   raise ImplementMe
-                     
+ *)                    
