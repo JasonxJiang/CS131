4d4f6199d0c5ef266ba2b1290782e898a5d7a872
Jason Jiang <jiang.jason.x@gmail.com>
2016-04-06 09:38:23 -0700

    first commit

---
+++ hw1.ml
@@ -0,0 +1,38 @@
+
+exception ImplementMe
+
+(* Problem 1 *)
+            
+let (member : 'a -> 'a list -> bool) =
+  raise ImplementMe
+
+let (add : 'a -> 'a list -> 'a list) =
+  raise ImplementMe
+
+let (union : 'a list -> 'a list -> 'a list) =
+  raise ImplementMe
+
+let (fastUnion : 'a list -> 'a list -> 'a list) =
+  raise ImplementMe
+                
+let (intersection : 'a list -> 'a list -> 'a list) =
+  raise ImplementMe
+                
+let (setify : 'a list -> 'a list) =
+  raise ImplementMe
+         
+let rec (powerset : 'a list -> 'a list list) =
+  raise ImplementMe
+
+        
+(* Problem 2 *)        
+        
+let (partition : ('a -> bool) -> 'a list -> 'a list * 'a list) =
+  raise ImplementMe
+
+let (whle : ('a -> bool) -> ('a -> 'a) -> 'a -> 'a) =
+  raise ImplementMe
+                                    
+let (pow : int -> ('a -> 'a) -> ('a -> 'a)) =
+  raise ImplementMe
+                     
